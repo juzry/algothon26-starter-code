8c5b5c4578f027252a58366baa39a273010fa4ae
rjusni <ryanjusni2005@gmail.com>
2026-07-18 11:13:59 +1000

    Push first mean reversion implementation

--- teamName.py
+++ teamName.py
@@ -5,11 +5,29 @@ currentPos = np.zeros(nInst)
 def getMyPosition (prcSoFar):
     global currentPos
     (nins,nt) = prcSoFar.shape
-    if (nt < 2):
+
+    long_window = 50
+    short_window = 20
+    if (nt < long_window):
         return np.zeros(nins)
-    lastRet = np.log(prcSoFar[:,-1] / prcSoFar[:,-2])
-    lNorm = np.sqrt(lastRet.dot(lastRet))
-    lastRet /= lNorm
-    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:,-1]])
+    
+    recent = prcSoFar[:,-long_window:]
+    short_rec = prcSoFar[:,-short_window:]
+    ma = recent.mean(axis=1)
+    sma = short_rec.mean(axis=1)
+
+    # lastRet = np.log(prcSoFar[:,-1] / prcSoFar[:,-2])
+    # lNorm = np.sqrt(lastRet.dot(lastRet))
+    # lastRet /= lNorm
+
+    last_price = prcSoFar[:,-1]
+    dev = ma - sma #dev is positive if current price is below average/should buy
+
+    norm = np.sqrt(dev.dot(dev)) 
+    if norm != 0:
+        dev /= norm
+
+
+    rpos = np.array([int(x) for x in 5000 * dev / prcSoFar[:,-1]])
     currentPos = np.array([int(x) for x in currentPos+rpos])
     return currentPos
